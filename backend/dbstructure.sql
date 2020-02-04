b298d842a6dc5839bbd058d77a2e7872192f7e83
Sam <sam.may.2412@gmail.com>
2020-02-04 14:53:48 +0000

    Built on the previous work, added the rest of the tables required

--- backend/dbstructure.sql
+++ backend/dbstructure.sql
@@ -19,3 +19,23 @@ CREATE TABLE IF NOT EXISTS Posts (
   post_likes SMALLINT NOT NULL,
   user_id varchar(8) not null REFERENCES Users(user_id)
 );
+
+CREATE TABLE IF NOT EXISTS Comments (
+  comment_id varchar(8) PRIMARY KEY,
+  comment_content TEXT NOT NULL,
+  user_id varchar(8) NOT NULL REFERENCES Users(user_id),
+  post_id varchar(8) NOT NULL REFERENCES Posts(post_id),
+  reply_id varchar(8) REFERENCES Comments(comment_id)
+);
+
+CREATE TABLE IF NOT EXISTS Reports_Posts (
+  report_post_id varchar(8) PRIMARY KEY,
+  report_count SMALLINT NOT NULL,
+  post_id varchar(8) NOT NULL REFERENCES Posts(post_id)
+);
+
+CREATE TABLE IF NOT EXISTS Reports_Comments (
+  report_comment_id varchar(8) PRIMARY KEY,
+  report_count SMALLINT NOT NULL,
+  comment_id varchar(8) NOT NULL REFERENCES Comments(comment_id)
+);
