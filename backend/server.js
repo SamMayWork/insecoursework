d651d2b4bdb82d992778c2c7645ec7ad15c9d6bf
Sam <sam.may.2412@gmail.com>
2020-02-06 12:04:57 +0000

    Added some more routes to the API, improved a little bit of documentation and did some refactoring

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@
 // /
 // / Main backend server file for the INSE coursework project
 // / This file starts an express server listening on the port 8080 for incoming connections
-// /
-// /
+// / Options for startup:
+// /  : --verbose, makes the startup procedure for the application verbose
+// /  : --logging, logs every HTTP request made to the server
+
+//////////////////////////////////////////////////////////////// ESLINT-DISABLES
 
 /* eslint-disable no-console */
 /* eslint-disable max-len */
@@ -28,7 +31,7 @@ app.use(bodyParser.json());
 // ////////////////////////////////////////////////////////////// COMMAND LINE ARGUMENTS
 
 if (argv.verbose) {
-  console.log(`${warnMessage}Starting the server`);
+  console.log(`${warnMessage}Starting the server...`);
 }
 
 // ////////////////////////////////////////////////////////////// API END POINT HANDLERS
@@ -39,9 +42,7 @@ if (argv.verbose) {
 // /forum/get?board=[param]&order=[param] - Gets a list of posts in a board in a given order
 // Since the forum allows unregistered users to access the site, there is no need for authentication here
 app.get('/forum/get', (req, res) => {
-  if (argv.logging) {
-    console.log(`${getMessage} ${req.originalUrl} ${Date.now()} ${req.ip}`);
-  }
+  handleLogging(req);
 
   if (req.query.thread !== undefined) {
     // handleThreadGet();
@@ -60,19 +61,63 @@ app.get('/forum/get', (req, res) => {
 // /forum/create?post=[param] - Create a comment using the provided information in the POST body, or
 //                               if the comment already exists, updates the comment with the edited content
 app.post('/forum/create', (req, res) => {
-  if (argv.logging) {
-    console.log(`${postMessage} ${req.originalUrl} ${Date.now()} ${req.ip}`);
+  handleLogging(req);
+
+  res.end();
+});
+
+// Handler for HTTP Posts incoming to "like" or "dislike" posts
+// /forum/like?like=[param]&post=[param] - If like==true then it likes the post with the given ID 
+// /forum/like?like=[param]&comment=[param] - If like==true then it likes the comment with the given ID
+// if like==false then it will dislike the associated post
+app.post('/forum/like', (req, res) => {
+  handleLogging(req);
+
+  if (req.query.like !== undefined && req.query.post != undefined) {
+    // handleLike();
+  }
+
+  if (req.query.like !== undefined && req.query.comment != undefined) {
+    // handleLike();
   }
 
   res.end();
 });
 
+// Handler for HTTP posts to the report system of the application
+// /forum/report?post=[param] - Reports a post using the given ID
+// /forum/report?comment=[param] - Reports a comment using the given ID
+app.post('forum/report', (req, res) => {
+  handleLogging(req);
+
+  if (req.query.post !== undefined) {
+    // handlePostReport();
+  }
+
+  if (req.query.comment !== undefined) {
+    // handleCommentReport();
+  }
+
+  res.end();
+});
 
 // //////////////////////////////////////////////////////////////
 
 // Catch-all for 404's
 app.get('*', (req, res) => {
+  handleLogging(req);
   res.end('Could not process request');
 });
 
 app.listen(8080);
+
+
+/**
+ * Handles the logging function whenever a request comes in to the sever
+ * @param {Request} req The Request for the server to handle 
+ */
+function handleLogging (req) {
+  if (argv.logging) {
+    console.logging(`${postMessage} ${req.originalUrl} ${Date.now()} ${req.ip}`);
+  }
+}
