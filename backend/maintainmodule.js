7648d9a4b228766c716f87f1fe73fe38a9e15878
Sam <sam.may.2412@gmail.com>
2020-02-07 14:10:43 +0000

    Most recent work

--- backend/maintainmodule.js
+++ backend/maintainmodule.js
@@ -3,14 +3,14 @@
 // This module is responsible for the cold-start procedure for the application
 // and is also repsonsible for creating all of the new boards anually
 
+const logging = require('./logging');
+const logging = require('./dbabstraction');
+
 // ////////////////////////////////////////////////////////////// ESLINT-DISABLES
 
 /* eslint-disable no-use-before-define */
 
 // ////////////////////////////////////////////////////////////// MAINTAIN MODULE
-
-const logging = require('./logging');
-
 
 function coldStart () {
   logging.coldStartMessage("Cold-start option selected, running cold-start procedure...");
