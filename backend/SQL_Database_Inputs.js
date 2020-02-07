0b77c72e46c63e515be6d1dff3e45cda1ed568eb
HarryPlane <harryplane1995@hotmail.co.uk>
2020-02-07 15:24:39 +0000

    Added an example that needs reviewing

---
+++ backend/SQL_Database_Inputs.js
@@ -0,0 +1,29 @@
+/**
+ * Attempts to get the title and content of a note using its ID
+ * @param {String} id The ID of the note to find in the database
+ * @param {*} sql An SQLConnection to pull the information from
+ */
+async function getNote(id, sql) {
+  console.log(Request for ${id});
+  const query = 'SELECT * FROM displaynotes WHERE note_id = $1';
+  const results = await sql.query(query, [id]);
+  return {
+    id: results.rows[0].note_id,
+    title: results.rows[0].note_title,
+    content: results.rows[0].note_content,
+  };
+}
+
+
+function getPost (postid, sql) {
+    console.log(Request for ${postid});
+    const query = 'select * from Posts WHERE post_id = postid;';
+    const results = await sql.query(query, [postid]);
+    return {
+        id: results.rows[0].post_id,
+        title: results.rows[0].post_title,
+        content: results.rows[0].post_content,
+        likes: results.rows[0].post_likes,
+        userid: results.rows[0].user_id,
+    };
+}
