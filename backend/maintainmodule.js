6f981c94e3d9e1e67648d40e8f34b285ba3eeaa3
Sam <sam.may.2412@gmail.com>
2020-02-07 16:49:22 +0000

    Minor changes to the coldstart module and added the postgres client to the npm packages

--- backend/maintainmodule.js
+++ backend/maintainmodule.js
@@ -4,16 +4,33 @@
 // and is also repsonsible for creating all of the new boards anually
 
 const logging = require('./logging');
-const logging = require('./dbabstraction');
+const dbabs = require('./dbabstraction');
 
 // ////////////////////////////////////////////////////////////// ESLINT-DISABLES
 
 /* eslint-disable no-use-before-define */
 
+// ////////////////////////////////////////////////////////////// BOARDS
+
+modules = [
+  "Introduction to Software Engineering",
+  "Data Structures and Algorithms",
+  "Computer Architecture",
+  "Web Foundations 1"
+];
+
 // ////////////////////////////////////////////////////////////// MAINTAIN MODULE
 
 function coldStart () {
   logging.coldStartMessage("Cold-start option selected, running cold-start procedure...");
+
+  logging.coldStartMessage('Starting to create boards...');
+
+  for (let imodule of modules) {
+    const yearString = `${new Date().getFullYear()}/${new Date().getFullYear()+1}`;
+    //dbabs.createBoard(imodule, yearString);
+    logging.coldStartMessage(`Created board for ${imodule} of year ${yearString}`);
+  }
 }
 
 module.exports.coldStart = coldStart;
