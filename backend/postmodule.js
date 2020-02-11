45bdb629706470fe4aa99a48b4923450c9ef7a7d
Sam <sam.may.2412@gmail.com>
2020-02-11 23:06:11 +0000

    Added the post module

---
+++ backend/postmodule.js
@@ -0,0 +1,9 @@
+// Created on 11/02/2020
+//
+// This is the module that is responsible for all of the actions related to
+// the post management system (PMS) inside of the system architecture
+
+const logging = require('./logging');
+const dbabs = require('./dbabstraction');
+
+// ////////////////////////////////////////////////////////////// POSTS
