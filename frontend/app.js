4a98b14878e93ddbdd49b8984408eb00d77d3904
Reece Birchmore <reecebirchmore@gmail.com>
2020-02-12 18:21:22 +0000

    Joint effort to decouple the navbar from other components

--- frontend/app.js
+++ frontend/app.js
@@ -4,22 +4,6 @@ function $(id) {
 	return document.getElementById(id);
 }
 
-class App {
-	constructor() {
-		this.title = "";
-		this.screens = [];
-	}
-	addScreen(screenObj) {
-		this.screens.push(screenObj);
-	}
-	display(parentElem) {
-		this.screens.forEach((screen) => {
-			parentElem.appendChild(screen.getParent());
-			screen.display(screen.getParent());
-		});
-	}
-}
-
 class Component {
 	constructor(title, parentElement) {
 		this.title = title;
@@ -46,6 +30,19 @@ class Component {
 	}
 }
 
+class App extends Component {
+	constructor(title) {
+		super(title);
+
+	}
+	display(root) {
+		this.elements.push(root);
+		this.components.forEach((component) => {
+			component.display(this.elements[0]);
+		});
+	}
+}
+
 class Screen extends Component {
 	constructor(title) {
 		super(title);
@@ -61,49 +58,85 @@ class Navbar extends Component {
 		this.display_title = display_title;
 		let elem = Component.createElem("div");
 		elem.id = "navbar-" + title;
+		let contentId = elem.id + "-nav-content";
 		elem.innerHTML = `
 			<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
 			mdl-layout--fixed-header">
 				<header class="mdl-layout__header">
-				<div class="mdl-layout__header-row">
-				<div class="mdl-layout-spacer"></div>
-				<div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-				  mdl-textfield--floating-label mdl-textfield--align-right">
-				<label class="mdl-button mdl-js-button mdl-button--icon"
-				for="fixed-header-drawer-exp">
-				<i class="material-icons">search</i>
-				</label>
-				<div class="mdl-textfield__expandable-holder">
-				<input class="mdl-textfield__input" type="text" name="sample"
-				 id="fixed-header-drawer-exp">
-				</div>
-				</div>
-				</div>
+					<div class="mdl-layout__header-row">
+					<div class="mdl-layout-spacer"></div>
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+					mdl-textfield--floating-label mdl-textfield--align-right">
+					<label class="mdl-button mdl-js-button mdl-button--icon"
+					for="fixed-header-drawer-exp">
+					<i class="material-icons">search</i>
+					</label>
+					<div class="mdl-textfield__expandable-holder">
+					<input class="mdl-textfield__input" type="text" name="sample"
+					id="fixed-header-drawer-exp">
+					</div>
+					</div>
+					</div>
 				</header>
 				<div class="mdl-layout__drawer">
-				<span class="mdl-layout-title">${display_title}</span>
-				<nav class="mdl-navigation">
-				<a class="mdl-navigation__link" href="">Dashboard</a>
-				<a class="mdl-navigation__link" href="">Module Finder</a>
-				<a class="mdl-navigation__link" href="">Account Settings</a>
-				</nav>
+					<span class="mdl-layout-title">${display_title}</span>
+					<nav class="mdl-navigation">
+						<a class="mdl-navigation__link" href="">Dashboard</a>
+						<a class="mdl-navigation__link" href="">Module Finder</a>
+						<a class="mdl-navigation__link" href="">Account Settings</a>
+					</nav>
 				</div>
 				<main class="mdl-layout__content">
-					<div class="page-content"><!-- Your content goes here --></div>
+					<div id="${contentId}" class="page-content"><!-- Your content goes here --></div>
 				</main>
 			</div>
 		`;
 		this.elements.push(elem);
 	}
-}
-
-/*
-class Card extends Component {
-	constructor() {
-		
+	display(parentElem) {
+		parentElem.appendChild(this.elements[0]);
+		let thisID = "navbar-" + this.title;
+		this.elements.push($(thisID + "-nav-content"));
+		this.components.forEach((component) => {
+			console.log(this.elements);
+			this.elements[1].appendChild(component.getParent());
+			component.display(this.elements[1]);
+		});
 	}
 }
 
+
+class Card extends Component {
+	constructor(title) {
+		super(title);
+		
+		let elem = Component.createElem("div");
+		elem.id = "card-" + title;
+		elem.innerHTML = `
+		<div class="demo-card-wide mdl-card mdl-shadow--2dp">
+			<div class="mdl-card__title">
+				<h2 class="mdl-card__title-text">Welcome</h2>
+			</div>
+			<div class="mdl-card__supporting-text">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Mauris sagittis pellentesque lacus eleifend lacinia...
+			</div>
+			<div class="mdl-card__actions mdl-card--border">
+				<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+				Get Started
+				</a>
+			</div>
+			<div class="mdl-card__menu">
+				<button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+				<i class="material-icons">share</i>
+			</button>
+			</div>
+		</div>`;
+		this.elements.push(elem);
+	}
+}
+
+/*
 class PostCard extends Card {
 	constructor() {
 		
