81af73dc9c7ff10029dcdab867a9b6c4133b1d94
Sam <sam.may.2412@gmail.com>
2020-03-02 17:14:47 +0000

    Potentially fixed tests using new function

--- backend/tests/servertests.js
+++ backend/tests/servertests.js
@@ -103,8 +103,8 @@ describe('dbabstraction Tests', function () {
 
   describe('createPost', function() {
     it('Should return the expected content for the row 7c367dd6', async function (){
-      insert into Keywords (keyword_id, keyword_1, keyword_2, keyword_3, keyword_4,keyword_5) VALUES ('864cce11', 'test', 'database', 'createPost');
-      insert into Posts (post_id, keyword_id, post_title, post_content, post_likes, user_id, created_date, edited_date) VALUES ('8w715ss9','864cce11', 'Testing createPost is fun','Ever wonder how a database is created? Filled? Have you ever wondered how its tested when its been made? Me too.',10                                ,'100bad41','2020-02-17','2020-02-15');
+      dbabs.executeRawQuerySync("insert into Keywords (keyword_id, keyword_1, keyword_2, keyword_3, keyword_4,keyword_5) VALUES ('864cce11', 'test', 'database', 'createPost');");
+      dbabs.executeRawQuerySync("insert into Posts (post_id, keyword_id, post_title, post_content, post_likes, user_id, created_date, edited_date) VALUES ('8w715ss9','864cce11', 'Testing createPost is fun','Ever wonder how a database is created? Filled? Have you ever wondered how its tested when its been made? Me too.',10,'100bad41','2020-02-17','2020-02-15');");
       let result = await dbabs.getPost('8w715ss9');
       assert.deepEqual(result, {
         post_id : '8w715ss9',
