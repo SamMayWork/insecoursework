530ee7e079efb36fc163b8e10a7b3106b2992d0f
Joe <joe.makepeace01@gmail.com>
2020-03-04 12:57:21 +0000

    Added basic routing and cleaned up landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,83 +1,26 @@
 import React, {Component} from 'react';
-import './App.css';
-import Comment from "./components/Comment";
-import Comments from "./components/Comments";
-import Navbar from "./components/Navbar";
-import NavbarLink from "./components/NavbarLink";
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    }
-  }
-  render() {
-    const {error, isLoaded, comments } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>
-    } else if (!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      console.log(comments);
-      return (
-        <div className = "App">
-          <Navbar>
-            <NavbarLink active>Newest</NavbarLink>
-            <NavbarLink>Featured</NavbarLink>
-            <NavbarLink>Frequent</NavbarLink>
-            <NavbarLink>Recent</NavbarLink>
-          </Navbar>
-          <Comments>
-            {comments.map((comment, i) => (
-              <Comment
-                key = {i}
-                title = {comment.title}
-                author = {comment.author}
-                replies = {comment.replies}
-                date = {comment.date}
-              />
-            ))}
-          </Comments>
-        </div>
-      );
-    }
-  }
-  componentDidMount() {
-    fetch("https://www.reddit.com/r/popular.json")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          let comments = [];
-          result.data.children.forEach((comment) => {
-            let commentData = comment.data;
-            let commentObj = {
-              title: commentData.title,
-              author: commentData.author,
-              replies: commentData.num_comments,
-              date: commentData.created * 1000
-            };
-            comments.push(commentObj);
-          })
-          this.setState({
-            isLoaded: true,
-            comments: comments
-          });
-          console.log(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
+export default function App() {
+  return (
+    <Router>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+    </Router>
+  );
 }
 
-export default App;
+function Login() {
+  return <div>Login</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
