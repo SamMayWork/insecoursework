b02f56260fed14f1a0674909287087fd3ca9646c
Joe <joe.makepeace01@gmail.com>
2020-03-06 16:36:34 +0000

    Finished account page mockup. No functionality at the moment.

--- frontend/src/containers/AccountPage/index.js
+++ frontend/src/containers/AccountPage/index.js
@@ -6,7 +6,7 @@
  */
 
 import React, {useState} from 'react';
-import {Card, CardHeader, CardContent, Divider, Grid, Button, TextField} from '@material-ui/core';
+import {Card, Checkbox, CardActions, CardHeader, CardContent, Divider, Grid, Button, TextField} from '@material-ui/core';
 
 import './accountPage.css';
 
@@ -16,23 +16,53 @@ const AccountDetails = props => {
     displayName: 'John Doe',
   });
   return (
+    <div>
     <Card>
       <CardHeader
         title="Account Page"
       />
-      <Divider/>
+    </Card>
+    <Divider/>
+    <Card>
+    <CardHeader
+        subheader="Privacy"
+      />
       <CardContent>
         <Grid container spacing={3}>
           <Grid item md={6} xs={12}>
-            <TextField
-              fullWidth
-              label="Display name"
-              values={values.firstName}
-            />
+            Display Real Name <Checkbox/>
           </Grid>
         </Grid>
       </CardContent>
     </Card>
+    <Divider/>
+    <Card>
+      <CardHeader
+        subheader="Notifications"
+      />
+      <CardContent>
+        <Grid container spacing={3}>
+          <Grid item md={6} xs={12}>
+            Receive Notifications <Checkbox/>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+    <Divider/>
+    <Card>
+      <CardHeader
+        subheader="Danger Zone"
+      />
+      <CardActions>
+        <Button
+          color="red"
+          variant="contained"
+        >
+          Delete account
+        </Button>
+      </CardActions>
+    </Card>
+    </div>
   )
 };
 
