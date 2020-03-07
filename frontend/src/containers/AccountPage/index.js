d25dba09773fda23550423048a485d0e44f8f2c9
Joe <joe.makepeace01@gmail.com>
2020-03-07 16:40:18 +0000

    Added authentication changes from authTest branch so only server-side of that branch needs to be integrated and then it won't be needed any more.

--- frontend/src/containers/AccountPage/index.js
+++ frontend/src/containers/AccountPage/index.js
@@ -6,70 +6,140 @@
  */
 
 import React, {useState} from 'react';
-import {Card, Checkbox, CardActions, CardHeader, CardContent, Divider, Grid, Button, TextField} from '@material-ui/core';
+import {
+  Card,
+  Checkbox,
+  CardActions,
+  CardHeader,
+  CardContent,
+  Divider,
+  Grid,
+  Button,
+  TextField,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
+import styled from 'styled-components';
 
 import './accountPage.css';
 
+/*
 const AccountDetails = props => {
   const {className, ...rest} = props;
   const {values, setValues} = useState({
     displayName: 'John Doe',
   });
   return (
-    <div>
-    <Card>
+    <div style={styling}>
+      <Card>
+        <CardHeader
+          title="Account Page"
+        />
+      </Card>
+      <Divider/>
+      <Card>
       <CardHeader
-        title="Account Page"
-      />
-    </Card>
-    <Divider/>
-    <Card>
-    <CardHeader
-        subheader="Privacy"
-      />
-      <CardContent>
-        <Grid container spacing={3}>
-          <Grid item md={6} xs={12}>
-            Display Real Name <Checkbox/>
+          subheader="Privacy"
+        />
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              Display Real Name <Checkbox/>
+            </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-    <Divider/>
-    <Card>
-      <CardHeader
-        subheader="Notifications"
-      />
-      <CardContent>
-        <Grid container spacing={3}>
-          <Grid item md={6} xs={12}>
-            Receive Notifications <Checkbox/>
+        </CardContent>
+      </Card>
+      <Divider/>
+      <Card>
+        <CardHeader
+          subheader="Notifications"
+        />
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              Receive Notifications <Checkbox/>
+            </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-    <Divider/>
-    <Card>
-      <CardHeader
-        subheader="Danger Zone"
-      />
-      <CardActions>
-        <Button
-          color="red"
-          variant="contained"
-        >
-          Delete account
-        </Button>
-      </CardActions>
-    </Card>
+        </CardContent>
+      </Card>
+      <Divider/>
+      <Card>
+        <CardHeader
+          subheader="Danger Zone"
+        />
+        <CardActions>
+          <Button
+            color="red"
+            variant="contained"
+          >
+            Delete account
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   )
+};
+*/
+
+const NotificationZone = props => {
+  return (
+    <div className="accountPageZone">
+      <CardHeader
+        title="Notifications"
+      />
+      <div>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="Active"
+              secondary="We will send notification emails to the listed address when someone responds to your post"
+            />
+          </ListItem>
+        </List>
+      </div>
+    </div>
+  )
+}
+
+const StyledButton = styled(Button)`
+  &:hover {
+    background: red;
+    color: white;
+  }
+`;
+
+const DangerZone = props => {
+  return (
+    <div className="accountPageZone">
+      <CardHeader
+        title="Danger Zone"
+      />
+      <div className="accountPageDanger">
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="Delete this account"
+              secondary="Once you delete an account, there is no going back. Please be certain."
+            />
+            <StyledButton
+              color="secondary"
+              variant="outlined"
+            >
+              Delete account
+            </StyledButton>
+          </ListItem>
+        </List>
+      </div>
+    </div>
+  );
 };
 
 export function AccountPage() {
   return (
     <div className = "accountPage">
-      <AccountDetails/>
+      <NotificationZone/>
+      <DangerZone/>
     </div>
   );
 }
--- frontend/src/containers/LoginPage/index.js
+++ frontend/src/containers/LoginPage/index.js
@@ -10,10 +10,20 @@ import GoogleLogin from 'react-google-login';
 
 import './loginPage.css';
 
-const clientID = "817279853236-n8lmn3pt96j7q4uh61ulgb7alnqgc1t8.apps.googleusercontent.com";
+const clientID = "817279853236-toe6rfq5ebg7rife6fvd82hh0eclpt3t.apps.googleusercontent.com";
 
-const responseGoogle = (response) => {
-  console.log(response);
+function responseGoogle(response) {
+  let token = response.uc.id_token;
+  console.log(response, token);
+  fetch('/auth/test', {
+    credentials: 'same-origin',
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  }).then((res) => console.log(res)).then(
+    (res) => console.log(res)
+  );
 }
 
 export function LoginPage() {
@@ -26,7 +36,6 @@ export function LoginPage() {
         buttonText="Login with Google"
         clientId={clientID}
         onSuccess={responseGoogle}
-        onFailure={responseGoogle}
       />
     </div>
   );
