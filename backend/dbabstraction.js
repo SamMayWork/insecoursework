b66cb86aa06af09af63ded53efa4a81b2608c3e9
Joao santos <com>
2020-03-13 12:13:32 +0000

    checkUserExists test

--- backend/dbabstraction.js
+++ backend/dbabstraction.js
@@ -391,7 +391,8 @@ async function increaseCommentViews(commentid) {
 async function checkUserExists(email) {
   const query = 'SELECT * FROM users WHERE user_email = $1;';
   const results = await executeQuery(query, [email]);
-  console.log(results.rows[0]);
+  console.log(results.rows[0].user_id);
+  console.log(results.rows[1];
   if (results !== undefined) {
     return results.rows[1];    
   } 
