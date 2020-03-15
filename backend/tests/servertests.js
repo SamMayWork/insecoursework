d8eca1e9a49ae33658387717f30c9a7d7f63b260
Sam <sam.may.2412@gmail.com>
2020-03-15 23:04:02 +0000

    Working on the UAC and started to add proper documentation

--- backend/tests/servertests.js
+++ backend/tests/servertests.js
@@ -244,6 +244,16 @@ describe('dbabstraction Tests', function () {
           user_email: 'awesomeemail@gmail.com'});
       });
     });
+
+  describe('checkUserExists', function () {
+    it('Should return true for the email sbaldock0@hostgator.com', async function () {
+      assert.equal(await dbabs.checkUserExists('sbaldock0@hostgator.com'), true);
+    });
+
+    it('Should return false for the email thisisfake@fake.com', async function () {
+      assert.equal(await dbabs.checkUserExists('thisisfake@fake.com'), false);
+    });
+  });
 });
 
 describe('Maintenance Module', () => {
@@ -257,16 +267,46 @@ describe('Maintenance Module', () => {
 describe('uac testing', function () {
   describe ('checkUserExists', function () {
     it('Should return true for the email sbaldock0@hostgator.com', async function () {
-      assert.deepEqual(await dbabs.checkUserExists('sbaldock0@hostgator.com'), {
-        id : 'a2367eab',
-        exists : true
-      });
+      assert.equal(await dbabs.checkUserExists('sbaldock0@hostgator.com'), true);
     });
 
     it('Should return false for the email fake@notinthedb.com', async function () {
-      assert.deepEqual(await dbabs.checkUserExists('fake@notinthedb.com'), {
-        exists : false
-      });
+      assert.equal(await dbabs.checkUserExists('fake@notinthedb.com'), false);
     });
   });
-})
+
+  describe ('getCommentAuthor', function () {
+    it('Should return the ID 75b6d7e5 for the row q1f3b99f', async function () {
+      assert.equal(await uac.getCommentAuthor('q1f3b99f'), '75b6d7e5');
+    });
+
+    it('Should return undefined if the row does not exist', async function () {
+      assert.equal(await uac.getCommentAuthor('DOESNOTE'), undefined);
+    });
+  });
+
+  describe ('getPostAuthor', function () {
+    it('Should return the ID acc45ba4 for the row 11f3b99f', async function () {
+      assert.equal(await uac.getPostAuthor('11f3b99f'), 'acc45ba4');
+    });
+
+    it('Should return undefined if the row does not exist', async function () {
+      assert.equal(await uac.getPostAuthor('DOESNOTE'), undefined);
+    });
+  });
+
+  describe ('getUsersID', function () {
+    it('Should return the correct ID for the email jgallelli1@weibo.com', async function () {
+      let req = {
+        user : {
+          emails : [
+            {
+              value : 'jgallelli1@weibo.com'
+            }
+          ]
+        }
+      };
+      assert.equal(await uac.getUsersID(req), '75b6d7e5');
+    });
+  });
+});
