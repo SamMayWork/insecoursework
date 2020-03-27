8ad0fa6f713b7fa45d36a9e07ea7c8d7a7e32c19
Sam <sam.may.2412@gmail.com>
2020-03-27 13:43:39 +0000

    Updated logging tests

--- backend/tests/loggingtesting.js
+++ backend/tests/loggingtesting.js
@@ -6,6 +6,12 @@
 const assert = require('assert');
 const logging = require('../logging');
 
+// ////////////////////////////////////////////////////////////// TEST FLOW
+
+// Checks to make sure it prints to the screen correctly and does not crash ✔️
+
+// ////////////////////////////////////////////////////////////// TEST SCRIPT
+
 describe('logging tests', () => {
   it('Should have all different text methods appear correctly', () => {
     logging.coldStartMessage('This should be a cold-start message!');
@@ -15,5 +21,16 @@ describe('logging tests', () => {
     logging.successMessage('This should be a success message!');
     logging.warningMessage('This should be a warning message!');
     assert.ok(true);
+  });
+
+  it('Should not crash at all', async () => {
+    assert.doesNotThrow(() => {
+      logging.coldStartMessage('This should be a cold-start message!');
+      logging.errorMessage('This should be an error message!');
+      logging.logHttpGetMessage({}, 'This should be a HTTP GET message!');
+      logging.logHttpPostMessage({}, 'This should be a HTTP POST message!');
+      logging.successMessage('This should be a success message!');
+      logging.warningMessage('This should be a warning message!');
+    });
   });
 });
